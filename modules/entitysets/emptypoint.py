1bb9608f2ba64273e8d0690c17612e856a625786
Connor Sherson <cppsherson@gmail.com>
2012-07-01 21:25:51 +1200

    Give EmptyPoint a None for sheetFileName to prevent load crash.

--- modules/entitysets/emptypoint.py
+++ modules/entitysets/emptypoint.py
@@ -31,6 +31,7 @@ class EmptyPoint( Entity ):
 	pureSensor = True
 	sheet = pygame.Surface( ( width, height ) ).convert_alpha()
 	sheet.fill( pygame.Color( 0, 0, 0, 0 ) )
+	sheetFileName = None
 	pygame.draw.rect( sheet, pygame.Color( 255, 0, 0, 255 ), pygame.Rect( 0, 0, width, height ), 2 )
 	def __init__( self, pos=[0,0], vel=[0,0], group=None ):
 		Entity.__init__( self, pos, [0,0], None, group, pygame.Rect( 0, 0, self.width, self.height ), animated=False )
